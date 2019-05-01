5185eccc960fc3d63d5f2cbae124804a74c04cf6
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-05-01 21:12:39 +0600

    Added Simple test case for front end

--- simpleapp/pages/tests.py
+++ simpleapp/pages/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/home')
+        self.assertTrue(response.status_code, 200)
+
+    def test_contact_page_status_code(self):
+        response = self.client.get('/about')
+        self.assertTrue(response.status_code, 200)
