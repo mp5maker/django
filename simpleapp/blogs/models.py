0e8ad885979478b8cca5dd213900080f2ffd5e56
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-05-05 02:53:32 +0600

    Added a Create Page for the blogs

--- simpleapp/blogs/models.py
+++ simpleapp/blogs/models.py
@@ -4,6 +4,8 @@ from django.utils.timezone import now
 
 from django.template.defaultfilters import slugify
 
+from django.urls import reverse
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(blank=True)
@@ -12,15 +14,25 @@ class Post(models.Model):
         on_delete=models.CASCADE
     )
     body = models.TextField()
-    created_at = models.DateTimeField(default=now, editable=False)
+    created_at = models.DateTimeField(editable=False)
     updated_at = models.DateTimeField()
 
     def __str__(self):
         return self.title[:50]
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
+        slug = slugify(self.title[:25])
+        slug_exists = Post.objects.filter(slug=slug).exists()
+        if slug_exists:
+            self.slug = slug + "-another"
+        else:
+            self.slug = slug
         if not self.id:
-            self.created_at = now()
-        self.updated_at = now()
+            if not self.created_at:
+                self.created_at = now()
+        if not self.updated_at:
+            self.updated_at = now()
         return super(Post, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('blogs:details', kwargs={"slug": str(self.slug)})
