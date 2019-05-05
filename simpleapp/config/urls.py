50f6a7c31e97fb27ef8228aa446344906b5217d0
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-05-05 22:37:15 +0600

    Added a signup page

--- simpleapp/config/urls.py
+++ simpleapp/config/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('blogs/', include('blogs.urls')),
     path('posts/', include('posts.urls')),
