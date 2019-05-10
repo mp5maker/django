e192dcbfcbd35ee2cc8409383c6098fa1817579c
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-05-11 02:40:24 +0600

    Added Most Commented Posts

--- blogging/blogs/templatetags/blog_tags.py
+++ blogging/blogs/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
 from django import template
 
+from django.db.models import Count
+
 from ..models import Post
 
 register = template.Library()
@@ -8,6 +10,14 @@ register = template.Library()
 def total_posts(name='total_posts'):
     return Post.published.count()
 
+@register.inclusion_tag('blogs/layouts/most_commented_posts.html')
+def show_most_commented_posts(count=5):
+    commented_posts = Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
+    return {
+        "commented_posts": commented_posts
+    }
 
 @register.inclusion_tag('blogs/layouts/latest_posts.html')
 def show_latest_posts(count=5):
