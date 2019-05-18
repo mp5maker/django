9fefc9b9c49578acac5b03b7cc21099e61cd9bcc
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-05-18 13:37:33 +0600

    Made the bookmarklet working

--- bookmarks/config/settings.py
+++ bookmarks/config/settings.py
@@ -66,7 +66,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'static', 'root'),
+            os.path.join(BASE_DIR, 'static'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
