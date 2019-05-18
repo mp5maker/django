129107f3cb19dfcde2ba38635f1eb448709f84b0
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-05-18 22:21:44 +0600

    Added a Contact Intermediary Model

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 from django.conf import settings
 
 class Profile(models.Model):
@@ -12,3 +14,32 @@ class Profile(models.Model):
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
+
+
+class Contact(models.Model):
+    user_from = models.ForeignKey(
+        User,
+        related_name="rel_from_set",
+        on_delete=models.CASCADE
+    )
+    user_to = models.ForeignKey(
+        User,
+        related_name="rel_to_set",
+        on_delete=models.CASCADE
+    )
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return '{} follows {}'.format(self.user_from, self.user_to)
+
+User.add_to_class(
+    'following',
+    models.ManyToManyField('self',
+        through=Contact,
+        related_name="followers",
+        symmetrical=False
+    )
+)
