d5ec200327aa043fef2318fe78d5e93bc1a3bb1a
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-05-26 23:09:13 +0600

    Added orders in shop project

--- myshop/config/settings.py
+++ myshop/config/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     # My App
     'common',
     'shop',
-    'cart'
+    'cart',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -73,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart'
             ],
         },
     },
